01740f393e79e6f04cedb7adffa4d26ff5cb70c9
Murilo S. Amaral <murimaral@gmail.com>
2022-11-23 21:16:59 -0300

    Implanta pontuação com base na branch do Yuri. Customiza mensagens de regra. Formata tela de jogo.

--- batalha_naval.py
+++ batalha_naval.py
@@ -16,7 +16,8 @@ class BatalhaNaval():
         self.popular_navios()
 
     def __str__(self):
-        return "\n========================================="+\
+        return  "\n"*80+\
+                "\n========================================="+\
                "\n                TABULEIRO                \n"+\
                 "=========================================\n"+\
                 str(self.tabuleiro)+\
@@ -49,8 +50,8 @@ class BatalhaNaval():
         coordenada = coordenada.strip()
         print(len(coordenada), coordenada)
         if len(coordenada) not in [2,3]:
+            print(self)
             print("Coordenada deve conter 1 letra e um numero\n")
-            print(self.tabuleiro)
             inp = input("Digite uma coordenada: ")
             return self.converter_coordenadas(inp)
         
@@ -58,14 +59,14 @@ class BatalhaNaval():
         numero = coordenada[1:]
 
         if letra.isnumeric() or not numero.isnumeric():
+            print(self)
             print("Coordenada inválida: Primeiro letra, em segundo número\n")
-            print(self.tabuleiro)
             inp = input("Digite uma coordenada: ")
             return self.converter_coordenadas(inp)
         
         if letra.lower() not in "abcdefghij" or int(numero) not in range(1,11):
+            print(self)
             print("Coordenada inválida, range excedido\n")
-            print(self.tabuleiro)
             inp = input("Digite uma coordenada: ")
             return self.converter_coordenadas(inp)
 
@@ -96,8 +97,9 @@ class BatalhaNaval():
         }
         
         if self.tabuleiro.iloc[y].at[x] in ['agua', 'NV', '[SP]']:
+            print(self)
             print("Escolha outra coordenada, ponto já revelado!!\n")
-            print(self.tabuleiro)
+            time.sleep(5)
             return False
 
         self.animar_mira()
@@ -106,26 +108,31 @@ class BatalhaNaval():
             self.animar_tiro()
             self.tabuleiro.iloc[y].at[x] = 'agua'
             msg_id = randint(0,2)
-            print(dict_msgs_erro[msg_id])
+            print("X"*(len(dict_msgs_erro[msg_id])-2)+dict_msgs_erro[msg_id]+"X"*(len(dict_msgs_erro[msg_id])-2))
             self.tentativas_restantes -= 1
 
         elif self.tabuleiro_gabarito.iloc[y].at[x] != '[SP]':
             self.animar_tiro()
             self.tabuleiro.iloc[y].at[x] = 'NV'
             msg_id = randint(0,2)
-            print(dict_msgs_acerto[msg_id])
+            print("*"*(len(dict_msgs_acerto[msg_id])-2)+dict_msgs_acerto[msg_id]+"*"*(len(dict_msgs_acerto[msg_id])-2))
+            print("+ 1 PONTO")
+            self.pontos+=1
             for navio in self.navios_plotados:
                 if (y,x) in navio:
                     navio.remove((y,x))
                     if len(navio) < 1:
+                        self.pontos+=3
                         time.sleep(1)
                         msg_id = randint(0,3)
-                        print(dict_msgs_afunda[msg_id])
+                        print("*"*(len(dict_msgs_afunda[msg_id])-2)+dict_msgs_afunda[msg_id]+"*"*(len(dict_msgs_afunda[msg_id])-2))
                         print(f'Voce AFUNDOU UM NAVIO. Navios restantes: {self.calcula_navios_restantes()}')
+                        print("+ 3 PONTOS")
+
         else:
             self.tabuleiro.iloc[y].at[x] = '[SP]'
             self.animar_sup()
-
+        time.sleep(6)
         return True
 
     @staticmethod                
@@ -136,20 +143,26 @@ class BatalhaNaval():
         time.sleep(0.5)
         print("E...\n")
         # suspense
+        print("      ..      ")
+        print("     ....     ")
+        print("  ..........  ")
+        print("..............")
         print("..............")
         print("..../----\....")
-        print(".../      \....")
-        print("...\      /....")
+        print(".../      \...")
+        print("...\      /...")
         print("....|    |....")
         print("....|    |....")
         print("....|____|....")
+        print("  ..........  ")
+
         time.sleep(3)
 
     @staticmethod
     def animar_tiro():
-        print("..../\/\/\....")
-        print("...< POOW >...")
-        print("....\/\/\/....")
+        print("  ../\/\/\..  ")
+        print("   < POOW >   ")
+        print("    \/\/\/    ")
         time.sleep(0.5)
         print("       O      ")
         time.sleep(0.3)
@@ -178,12 +191,13 @@ class BatalhaNaval():
             1: "Uma caixa de munição no meio do oceano? Suspeito... Mas e daí, ela é sua agora!\n",
             2: "Hoje é seu dia de sorte! Uma caixa de munição toda para você!\n"
         }
-        print("ALTO LÁ!")
+        print("    ......    ")
+        print(">>>ALTO LÁ!<<<\n")
         time.sleep(1)
-        print("Você avista uma caixa muito chamativa boiando...")
+        print("Você avista uma caixa muito chamativa boiando...\n")
         time.sleep(2)
         msg_id = randint(0,2)
-        print(f"{dict_msg_mun[msg_id]}")
+        print("*"*(len(dict_msg_mun[msg_id])-2)+dict_msg_mun[msg_id]+"*"*(len(dict_msg_mun[msg_id])-2))
         print(".......................")
         print(".....============......")
         print("....||          ||.....")
@@ -193,9 +207,9 @@ class BatalhaNaval():
         print(".......................\n")
 
 
-        time.sleep(2)
+        time.sleep(1)
         print("Você recebe mais 3 tentativas!!")
-        self.tentativas_restantes+=3    
+        self.tentativas_restantes+=3
 
     # Popular aleatoriamente o tabuleiro criado com navios (Porta-Aviões, Cruzador, Destroyer) 
     def popular_porta_avioes(self):
@@ -331,8 +345,8 @@ class BatalhaNaval():
     def popular_municao(self):
 
         nivel = {
-            'facil': 5,
-            'normal': 3
+            'facil': 8,
+            'normal': 4
         }
 
         municao_restante = nivel.get(self.dificuldade, 0)
